c268d1b16d2160ca6e23de76925712e144202f89
José Costa <jpa1costa@gmail.com>
2013-08-30 19:50:16 +0100

    XML/JSON Response implemented

--- registry_service.php
+++ registry_service.php
@@ -7,42 +7,54 @@ define("SERVICE_NAME", "web_install_");
 if(isset($_REQUEST['token']) && isset($_REQUEST['device_id']) && isset($_REQUEST['mode']))
 {
 
-        $username = $_REQUEST['token'];
-        $d_imei = $_REQUEST['device_id'];
-        // XMl is the default response format                                                                                                  
+        // Request Parameters
+        $token = $_REQUEST['token'];
+        $device_id = $_REQUEST['device_id'];
+        // XMl is the default response format                                                            
         $mode  = strtolower($_REQUEST['mode']) == 'json' ? 'json' : 'xml';
 
-
+	//Response Parameters
+	$status;
+	$error= '';
+	
         // DATABASE AND RABBITMQ CONNECTIONS
         $db = pg_connect("host=localhost port=5432 dbname=register user=postgres password=godIsAProgrammer") or die('Connection to database failed: ' . pg_last_error());
 
         $publisher = new Publisher();
 
-        $queue_name = generateQueueName($username, $d_imei);
+        $queue_id = generateQueueName($username, $device_id);
 
         // PREPARED STATEMENT AND INSERTION
 	$p_statement = "INSERT INTO queue (device_imei, username, queue_name) VALUES ($1, $2, $3)";
 	pg_prepare('register_query', $p_statement);
-	pg_execute('register_query', array($d_imei, $username, $queue_name)) or die('Error while inserting!');
+	pg_execute('register_query', array($device_id, $token, $queue_id)) or die('Error while inserting!');
 
 	// CREATING RABBITMQ QUEUE
-	$publisher->create_queue($queue_name);
+	$publisher->create_queue($queue_id);
 
 	$publisher->close();
 	pg_close($db);
 
-	$response = array();
+	$status = 'OK';
 
-	if($format == 'json')
+	$response = array();
+	$response['status'] = $status;
+	$response['queue_id'] = $queue_id;
+	$response['errors'] = $error;
+	
+	if($mode == 'json')
 	  {
-	    sendResponse(200, 'OK', json_encode($queue_name), 'application/json');
+	    sendResponse(200, 'OK', json_encode($response), 'application/json');
 	  }
 	else
 	  {
-	    sendResponse(200, 'OK', SimpleXMLElement($queue_name), 'text/xml');
+	    $xml = new SimpleXMLElement('<?xml version="1.0" encoding="UTF-8"?><Response />');
+	    foreach($response as $key => $value) 
+	    {
+	      $xml->addChild($key, $value);
+	    }    
+	    sendResponse(200, 'OK', $xml->asXML(), 'text/xml');
 	  }
-
-	print "Registry Service Closed!";
 }
 else
 {
